9bbf72af54c8c2077ffd88d9d0a4f865ffdbb146
chkim888 <chkimm888@gmail.com>
2026-06-02 20:59:08 -0700

    register auth router in main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from app.routers.auth import router as auth_router
 
 # Initialize the main web app object -- this orchestrates the entire API
 app = FastAPI()
+
+### routers
+app.include_router(auth_router)
 
 # test endpoint -- just to see if things work
 @app.get("/")
