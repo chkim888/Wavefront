dfbb04589c54899a333f4cad0389af8b99c5c348
chkim888 <chkimm888@gmail.com>
2026-06-07 23:14:01 -0700

    Changes for post schema

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -5,6 +5,11 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from app.database import Base
+from app.models.buzz_monitor import *
+from app.models.experiment import *
+from app.models.user import *
+
 import os
 from dotenv import load_dotenv
 
@@ -29,7 +34,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
