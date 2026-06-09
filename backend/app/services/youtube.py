4ac367628e0c933c10549e9b7e0e5d42740a39bb
chkim888 <chkimm888@gmail.com>
2026-06-09 00:31:45 -0700

    Reordering functions & sentiment analysis function reference added

--- backend/app/services/youtube.py
+++ backend/app/services/youtube.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from googleapiclient import discovery
 from googleapiclient.errors import HttpError
 from app.models.buzz_monitor import Post
+from app.services.sentiment import run_sentiment_analysis
 
 # Load env variables
 load_dotenv()
@@ -11,10 +12,53 @@ YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY")
 # Other constants
 MAX_RESULTS = 10
 
-# Return youtube service object using discovery.build function
-def get_youtube_client():
-    youtube = discovery.build('youtube', 'v3', developerKey=YOUTUBE_API_KEY)
-    return youtube
+# Ingest youtube data -- the whole process
+def ingest_youtube_data(topic_id, project_id, platform_id, keywords, db_session):
+    # search videos
+    video_ids = search_videos(keywords)
+    # get metadata & comments
+    metadata = get_video_metadata(video_ids)
+    comments = list()
+    for id in video_ids:
+        try:
+            new_comments = get_video_comments(id)
+            comments += new_comments
+        except HttpError:
+            continue # Skip videos without comments
+    # add data to the database
+    # Add post metadata
+    for post in metadata:
+        new_post = Post(
+            topic_id = topic_id,
+            project_id = project_id,
+            platform_id = platform_id,
+            original_poster = post["original_poster"],
+            posted_time = post["posted_time"],
+            content_type = post["content_type"],
+            content = post["content"],
+            view_count = post["view_count"],
+            like_count = post["like_count"],
+            comment_count = post["comment_count"]
+        )
+        db_session.add(new_post)
+    # Add comments
+    for comment in comments:
+        new_comment = Post(
+            topic_id = topic_id,
+            project_id = project_id,
+            platform_id = platform_id,
+            original_poster = comment["original_poster"],
+            posted_time = comment["posted_time"],
+            content_type = comment["content_type"],
+            content = comment["content"],
+            view_count = comment["view_count"],
+            like_count = comment["like_count"],
+            comment_count = comment["comment_count"]
+        )
+        db_session.add(new_comment)
+    db_session.commit()
+    # Call function to run sentiment analysis on the ingested posts
+    run_sentiment_analysis(topic_id, project_id, db_session)
 
 # Search videos associated with the keywords & return their IDs
 def search_videos(keywords: list[str], max_results: int = MAX_RESULTS):
@@ -32,6 +76,11 @@ def search_videos(keywords: list[str], max_results: int = MAX_RESULTS):
         new_ids = [item['id']['videoId'] for item in response.get('items', [])]
         id_list = id_list + new_ids
     return id_list
+
+# Return youtube service object using discovery.build function
+def get_youtube_client():
+    youtube = discovery.build('youtube', 'v3', developerKey=YOUTUBE_API_KEY)
+    return youtube
 
 # Return metadata from the given list of video IDs
 def get_video_metadata(video_ids: list[str]):
@@ -83,49 +132,3 @@ def get_video_comments(video_id: str, max_results: int = MAX_RESULTS):
         )
         metadata.append(new_metadata)
     return metadata
-
-# Ingest youtube data -- the whole process
-def ingest_youtube_data(topic_id, project_id, platform_id, keywords, db_session):
-    # search videos
-    video_ids = search_videos(keywords)
-    # get metadata & comments
-    metadata = get_video_metadata(video_ids)
-    comments = list()
-    for id in video_ids:
-        try:
-            new_comments = get_video_comments(id)
-            comments += new_comments
-        except HttpError:
-            continue # Skip videos without comments
-    # add data to the database
-    # Add post metadata
-    for post in metadata:
-        new_post = Post(
-            topic_id = topic_id,
-            project_id = project_id,
-            platform_id = platform_id,
-            original_poster = post["original_poster"],
-            posted_time = post["posted_time"],
-            content_type = post["content_type"],
-            content = post["content"],
-            view_count = post["view_count"],
-            like_count = post["like_count"],
-            comment_count = post["comment_count"]
-        )
-        db_session.add(new_post)
-    # Add comments
-    for comment in comments:
-        new_comment = Post(
-            topic_id = topic_id,
-            project_id = project_id,
-            platform_id = platform_id,
-            original_poster = comment["original_poster"],
-            posted_time = comment["posted_time"],
-            content_type = comment["content_type"],
-            content = comment["content"],
-            view_count = comment["view_count"],
-            like_count = comment["like_count"],
-            comment_count = comment["comment_count"]
-        )
-        db_session.add(new_comment)
-    db_session.commit()
