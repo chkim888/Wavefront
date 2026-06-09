b2f1bb2db3ffb5b7de39b922849e03b6e7d5169c
chkim888 <chkimm888@gmail.com>
2026-06-08 21:39:22 -0700

    user authorization step added for youtube ingestion

--- backend/app/routers/ingest.py
+++ backend/app/routers/ingest.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter, Depends, HTTPException, status
-from sqlalchemy import select
+from sqlalchemy import select, and_
 from uuid import UUID
 from app.models.buzz_monitor import Platform, Keyword
+from app.models.user import User_Project
 from app.database import get_db_session
+from app.auth.dependencies import get_current_user
 from app.services.youtube import ingest_youtube_data
 
 # Constant initialization
@@ -11,12 +13,20 @@ router = APIRouter(prefix="/ingest")
 
 # Ingest youtube data by calling ingest_youtube_data() services.youtube
 @router.post("/youtube/{topic_id}/{project_id}", status_code=status.HTTP_200_OK) # does this account for no 
-def ingest_youtube_endpoint(topic_id: UUID, project_id: UUID, db_session=Depends(get_db_session)):
-    # Is this going to be user-facing? 
+def ingest_youtube_endpoint(topic_id: UUID, project_id: UUID, user=Depends(get_current_user), db_session=Depends(get_db_session)):
+    # Check if the user is authorized for the project
+    authorized = db_session.scalars(
+        select(User_Project).where(
+            and_(User_Project.user_id == user.id,
+                 User_Project.project_id == project_id))).first()
+    if not authorized:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="User does not have access for project")
+    # Fetch YouTube platform ID
     platform_id = db_session.scalars(
         select(Platform.id).where(Platform.name == YOUTUBE)).first()
     if not platform_id:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Platform ID not found for YouTube")
+    # Run the ingestion function
     keywords = db_session.scalars( # handle keyword number limit somehow later 
         select(Keyword.keyword).where(Keyword.topic_id == topic_id)).all()
     if not keywords:
